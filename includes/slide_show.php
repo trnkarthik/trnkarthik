c17bac3bf6660d3413f5d228a1796e64b00f632a
Karthik-PC <karthik.trn@gmail.com>
2013-06-10 14:16:44 -0400

    Slideshow updated
    Slideshow updated

--- includes/slide_show.php
+++ includes/slide_show.php
@@ -6,9 +6,24 @@ top: -15%;
 width: 325px;
 height: 550px;
 }
-.color-1 {
+.color-1{
 background-color: #E3D8FF;
 background-color: rgba(139, 139, 139, 1);
+}
+.color-2{
+background-color: #1E9DB9;
+background-color: rgb(85, 128, 138);
+}
+.color-3{
+background-color: rgb(175, 94, 31);
+background-color: rgb(153, 98, 55);
+background-color: rgba(20, 82, 35, 1);
+}
+.color-4{
+background-color: rgb(153, 98, 55);
+}
+.color-5{
+background-color: rgba(99, 36, 36, 1);
 }
 </style>
 <section id="jms-slideshow" class="jms-slideshow">
@@ -27,7 +42,7 @@ background-color: rgba(139, 139, 139, 1);
 	    <p>But as the riper should by time decease, his tender heir might bear his memory</p>
 	    <a class="jms-link" href="#">Read more</a>
 	</div>
-	<img src="images/2.png" />
+	<img src="http://localhost/trnkarthik/images/BMIApplication_with_shadow/1.png" />
     </div>
                 
     <div class="step" data-color="color-3" data-x="2000" data-y="1500" data-z="1000" data-rotate="20">
@@ -36,7 +51,7 @@ background-color: rgba(139, 139, 139, 1);
 	    <p>Within thine own bud buriest thy content and, tender churl, makest waste in niggarding</p>
 	    <a class="jms-link" href="#">Read more</a>
 	</div>
-	<img src="images/3.png" />
+	<img src="http://localhost/trnkarthik/images/BMIApplication_with_shadow/1.png" />
     </div>
 	
     <div class="step" data-color="color-4" data-x="3000" data-y="2000">
@@ -45,7 +60,7 @@ background-color: rgba(139, 139, 139, 1);
 	    <p>Making a famine where abundance lies, thyself thy foe, to thy sweet self too cruel</p>
 	    <a class="jms-link" href="#">Read more</a>
 	</div>
-	<img src="images/4.png" />
+	<img src="http://localhost/trnkarthik/images/BMIApplication_with_shadow/1.png" />
     </div>
     				
     <div class="step" data-color="color-5" data-x="4000" data-y="1500" data-z="1000" data-rotate="-20">
@@ -54,7 +69,7 @@ background-color: rgba(139, 139, 139, 1);
             <p>Thou that art now the world's fresh ornament and only herald to the gaudy spring</p>
 	    <a class="jms-link" href="#">Read more</a>
         </div>
-	<img src="images/5.png" />
+	<img src="http://localhost/trnkarthik/images/BMIApplication_with_shadow/1.png" />
     </div>
                 
     </section>
